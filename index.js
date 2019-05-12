e87b002d7ff0d75acd9f7dfcd30577c4459be073
Edward <edylennyy@gmail.com>
2019-05-12 18:01:35 +0300

    initial

---
+++ index.js
@@ -0,0 +1,22 @@
+import { TweenMax } from "gsap"
+
+const box = document.createElement("div")
+box.setAttribute("class", "box")
+document.body.appendChild(box)
+
+box.addEventListener("mouseover", () => {
+  TweenMax.to(box, 0.25, { className: "+=hover" })
+})
+
+box.addEventListener("mouseout", () => {
+  TweenMax.to(box, 0.25, { className: "-=hover" })
+})
+
+box.addEventListener("mousedown", () => {
+  TweenMax.to(box, 0.25, { className: "+=down" })
+})
+
+box.addEventListener("mouseup", () => {
+  TweenMax.to(box, 0.25, { className: "-=down" })
+})
+
